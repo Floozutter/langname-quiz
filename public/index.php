fe65094748badc1e797ff51925b10f4f99d00565
Floozutter <floozutter@gmail.com>
2020-07-06 01:59:32 -0700

    Start HTML

--- public/index.php
+++ public/index.php
@@ -1,1 +1,11 @@
-This is the index page.
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>? - Quiz</title>
+</head>
+<body>
+	<div id="quiz-box">
+	</div>
+</body>
