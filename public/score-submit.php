dc58b5860976d16dcc8cc1e3e59877df7aa79d09
Floozutter <floozutter@gmail.com>
2020-07-07 16:18:52 -0700

    Fix navbar

--- public/score-submit.php
+++ public/score-submit.php
@@ -70,20 +70,23 @@
 </head>
 <body id="override">
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<a class="navbar-brand" href="/">Langalike</a>
+		<a class="navbar-brand" href="index.php">Langalike</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav mr-auto">
 				<li class="nav-item">
-					<a class="nav-link" href="/">Quiz</span></a>
+					<a class="nav-link" href="index.php">Quiz</span></a>
 				</li>
 				<li class="nav-item">
 					<a class="nav-link" href="about.php">About</a>
 				</li>
 				<li class="nav-item">
 					<a class="nav-link" href="scoreboard.php">Scoreboard</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="scoreboard.php">User</a>
 				</li>
 			</ul>
 		</div>
