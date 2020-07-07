3def0ce81aaaa8c9f75049d2f3ee5b951f542889
Floozutter <floozutter@gmail.com>
2020-07-07 05:22:50 -0700

    Add Bootstrap styling

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 	session_start();
+	
 ?>
 <!DOCTYPE html>
 <html>
@@ -9,16 +10,47 @@
 	<title>? - Quiz</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="css/quiz.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 	<script defer src="js/ajax.js"></script>
 	<script defer src="js/quiz.js"></script>
+	<script defer src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script defer src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </head>
 <body>
-	<div id="quiz">
-		<div id="prompt">
-			Hello, World!
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<a class="navbar-brand" href="/">?</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="/">Quiz<span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="about.php">About</a>
+				</li>
+			</ul>
 		</div>
-		<div id="choices"></div>
-		<button id="butty" type="button" disabled>Next</button>
-		<div id="progress"></div>
+	</nav>
+<div class="container align-items-center justify-content-center">
+	<div id="quiz" class="card mx-auto my-4">
+		<div class="card-header text-center">
+			This is the quiz!
+		</div>
+		<div class="card-body">
+			<div id="prompt" class="card-title mb-4 lead">
+				Hello, World! This text should introduce the quiz!
+			</div>
+			<div id="choices" class="list-group"></div>
+			<div class="col-12 mt-4">
+				<button id="butty" type="button" disabled class="btn btn-primary">Continue</button>
+			</div>
+		</div>
+		<div class="card-footer text-muted">
+			Progress: <span id="progress">0 of ?</span>
+			| Score: <span id="score">0</span>
+		</div>
 	</div>
 </body>
--- public/js/quiz.js
+++ public/js/quiz.js
@@ -33,16 +33,19 @@ function setQuestion(response) {
 	choicesDiv.innerHTML = "";
 	question.choices.forEach((c, i) => {
 		const cDiv = document.createElement("div");
-		cDiv.classList.add("choice");
+		cDiv.classList.add("choice", "list-group-item");
+		cDiv.classList.add();
 		cDiv.dataset.index = i;
 		cDiv.innerHTML = c.content;
 		choicesDiv.appendChild(cDiv);
 		cDiv.addEventListener("click", choiceListener, { once: true });
 	});
+	const progressSpan = document.querySelector("#progress");
+	progressSpan.textContent = `${CURR+1} of ${COUNT}`;
 }
 
 function choiceListener(e) {
-	e.target.classList.add("selected");
+	e.target.classList.add("selected", "list-group-item-primary");
 	document.querySelectorAll(".choice").forEach(cDiv => {
 		cDiv.removeEventListener("click", choiceListener);
 	});
@@ -56,21 +59,33 @@ function processAnswer(response) {
 	const question = JSON.parse(response);
 	document.querySelectorAll(".choice").forEach(cDiv => {
 		const index = parseInt(cDiv.dataset.index, 10);
+		const correct = question.choices[index].correct;
 		cDiv.classList.add(
-			question.choices[index].correct
-			? "correct"
-			: "incorrect"
+			correct
+			? cDiv.classList.add("correct")
+			: cDiv.classList.add("incorrect")
 		);
-		const explanation = document.createElement("p");
+		const badge = document.createElement("span");
+		badge.classList.add(
+			"badge",
+			correct ? "badge-success" : "badge-danger",
+			"badge-pill",
+			"float-right"
+		);
+		badge.textContent = correct ? '\u2713' : '\u2717';
+		cDiv.appendChild(badge);
+		const explanation = document.createElement("div");
+		explanation.classList.add("text-muted");
 		explanation.innerHTML = question.choices[index].explain;
 		cDiv.appendChild(explanation);
-		
 	});
 	const selected = document.querySelector(".choice.selected");
 	const selectedIndex = parseInt(selected.dataset.index, 10);
 	if (question.choices[selectedIndex].correct) {
 		SCORE += 1;
 	}
+	const scoreSpan = document.querySelector("#score");
+	scoreSpan.textContent = `${SCORE}`;
 	enableButty();
 }
 
